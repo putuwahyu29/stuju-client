68aa191bc1d6331c7b216fad65b9aea9c9686138
I Putu Agus Wahyu Dupayana <75474419+putuwahyu29@users.noreply.github.com>
2024-01-29 08:03:39 +0800

    Update

--- src/main.js
+++ src/main.js
@@ -5,15 +5,14 @@ import App from "./App.vue";
 import Axios from "axios";
 import store from "./store";
 
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "sweetalert2/dist/sweetalert2.min.css";
 
-import setupInterceptors from './services/setupInterceptors';
+import setupInterceptors from "./services/setupInterceptors";
 
-Axios.defaults.baseURL = "https://stuju-api.herokuapp.com/api";
+Axios.defaults.baseURL = "http://localhost:8090/";
 
 setupInterceptors(store);
 const app = createApp(App);
